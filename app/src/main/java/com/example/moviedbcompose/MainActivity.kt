2d3053143a437e1dd53e7c15b280eb6d64af84dd
lwin lwin cho <lwinlwincho135324@gmail.com>
2023-10-17 23:40:18 +0630

    Initial commit

---
+++ app/src/main/java/com/example/moviedbcompose/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.moviedbcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import com.example.moviedbcompose.ui.theme.MovieDBComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MovieDBComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ItemView(R.drawable.dummy)
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun ItemView(image: Int, modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = image),
+        contentDescription = null,
+        modifier = modifier
+            .size(10.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ItemPreview() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        ItemView(R.drawable.dummy)
+    }
+}
+
+class ThemePreviewParameterProvider : PreviewParameterProvider<Boolean> {
+    override val values: Sequence<Boolean> = sequenceOf(false, true)
+
+}
