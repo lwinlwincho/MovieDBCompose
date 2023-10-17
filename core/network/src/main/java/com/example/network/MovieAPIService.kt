51b4baa24fe97027506a99f9e18de518956074ee
lwin lwin cho <lwinlwincho135324@gmail.com>
2023-10-17 23:44:19 +0630

    add : api service and provide retrofit within NetworkModule

---
+++ core/network/src/main/java/com/example/network/MovieAPIService.kt
@@ -0,0 +1,39 @@
+package com.example.network
+
+import com.example.model.CreditModel
+import com.example.model.MovieDetailModel
+import com.example.model.MovieModel
+import com.example.model.MoviesResponseModel
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+const val API_KEY = "9c9e4b9082cd70edd1ed7afab4f198b6"
+
+const val MOVIE_BASE_URL = "https://api.themoviedb.org/3/movie/"
+
+const val IMAGE_URL = "https://image.tmdb.org/t/p/w500/"
+
+interface MovieAPIService {
+
+    @GET("now_playing")
+    suspend fun getNowPlaying(@Query("api_key") apiKey: String = API_KEY):MoviesResponseModel<MovieModel>
+
+    @GET("popular")
+    suspend fun getPopular(@Query("api_key") apiKey: String = API_KEY): MoviesResponseModel<MovieModel>
+
+    @GET("{movie_id}")
+    suspend fun loadMovieDetail(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String = API_KEY
+    ): MovieDetailModel?
+
+    @GET("{movie_id}/credits")
+    suspend fun getCredits(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String = API_KEY
+    ): CreditModel
+
+}
