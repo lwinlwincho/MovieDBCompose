3f78968b6052141c11c12ab318bb6e17b308c7c2
lwin lwin cho <lwinlwincho135324@gmail.com>
2023-10-22 17:24:00 +0630

    add : Repository and RepositoryModule for dagger hilt

---
+++ core/data/src/main/java/com/example/model/repository/MovieRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.example.model.repository
+
+import com.example.model.MovieModel
+import com.example.model.MoviesResponseModel
+import com.example.model.datasource.RemoteDataSource
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class MovieRepositoryImpl @Inject constructor(
+    private val remoteDataSource: RemoteDataSource,
+) : MovieRepository {
+
+    override val nowShowingMoviesFlow: Flow<MoviesResponseModel<MovieModel>>
+        get() = remoteDataSource.nowShowingMoviesFlow
+
+    override val popularMoviesFlow: Flow<MoviesResponseModel<MovieModel>>
+        get() = remoteDataSource.popularMoviesFlow
+
+    override fun getNowShowingMovies(): Flow<MoviesResponseModel<MovieModel>> {
+        return remoteDataSource.getNowPlaying()
+    }
+
+    override fun getPopularMovies(): Flow<MoviesResponseModel<MovieModel>> {
+        return remoteDataSource.getPopular()
+    }
+}
---
+++ core/domain/src/main/java/com/example/domain/repository/MovieRepository.kt
@@ -0,0 +1,16 @@
+package com.example.model.repository
+
+import com.example.model.MovieModel
+import com.example.model.MoviesResponseModel
+import kotlinx.coroutines.flow.Flow
+
+interface MovieRepository {
+
+    //for combine state flow
+    val nowShowingMoviesFlow : Flow<MoviesResponseModel<MovieModel>>
+    val popularMoviesFlow: Flow<MoviesResponseModel<MovieModel>>
+
+    //for state flow
+    fun getNowShowingMovies(): Flow<MoviesResponseModel<MovieModel>>
+    fun getPopularMovies(): Flow<MoviesResponseModel<MovieModel>>
+}
