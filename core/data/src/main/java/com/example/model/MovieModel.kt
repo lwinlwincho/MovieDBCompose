7786679c983f1944043e1b157a06f404f509f4a9
lwin lwin cho <lwinlwincho135324@gmail.com>
2023-10-22 17:10:26 +0630

    add : data model

--- core/data/src/main/java/com/example/model/MovieModel.kt
+++ core/data/src/main/java/com/example/model/MovieModel.kt
@@ -1,9 +1,10 @@
 package com.example.model
 
 import kotlinx.serialization.SerialName
-import java.io.Serializable
+import kotlinx.serialization.Serializable
 
 
+@Serializable
 data class MovieModel(
     @SerialName(value = "id") val id: Int,
 //poster path can string or null
@@ -11,5 +12,5 @@ data class MovieModel(
     @SerialName(value = "title") val title: String,
     @SerialName(value = "release_date") val releaseDate: String,
     @SerialName(value = "vote_average") val voteAverage: Double
-) : Serializable
+)
 
