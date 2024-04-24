639d60ee48bf3aedbc915e1824f3627121ebcd9f
lwin lwin cho <lwinlwincho135324@gmail.com>
2024-04-24 11:32:57 +0630

    hide : navigation bottom bar when reach detail

--- app/src/main/java/com/lwinlwincho/moviedbcompose/MainScreen.kt
+++ app/src/main/java/com/lwinlwincho/moviedbcompose/MainScreen.kt
@@ -2,36 +2,31 @@ package com.lwinlwincho.moviedbcompose
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.lwinlwincho.moviedbcompose.detail.DetailScreen
-import com.lwinlwincho.moviedbcompose.home.HomeScreen
 import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.lwinlwincho.moviedbcompose.detail.DetailScreen
 import com.lwinlwincho.moviedbcompose.favourite.FavouriteScreen
-import com.lwinlwincho.moviedbcompose.home.HomeViewModel
+import com.lwinlwincho.moviedbcompose.home.HomeScreen
 import com.lwinlwincho.moviedbcompose.ui.theme.MovieDBComposeTheme
 
 @Composable
@@ -39,46 +34,19 @@ fun MainScreen() {
 
     val navController = rememberNavController()
 
-    val bottomNavRouteList = listOf(
-        BottomNavRoute.Home,
-        BottomNavRoute.Favourite
-    )
+    var showBottomBar by rememberSaveable { mutableStateOf(true) }
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+    showBottomBar = when (navBackStackEntry?.destination?.route) {
+        "detail/{movieId}" -> false
+        else -> true
+    }
 
     MovieDBComposeTheme {
         Scaffold(
             bottomBar = {
-                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                val currentDestination = navBackStackEntry?.destination
-
-                NavigationBar {
-                    bottomNavRouteList.forEach { screen ->
-                        NavigationBarItem(
-                            selected = currentDestination?.route == screen.route,
-                            icon = {
-                                Icon(
-                                    imageVector = getBottomNavIconForScreen(screen = screen.route),
-                                    contentDescription = screen.route
-                                )
-                            },
-                            label = { Text(text = screen.route) },
-                            onClick = {
-                                navController.navigate(screen.route) {
-                                    // Pop up to the start destination of the graph to
-                                    // avoid building up a large stack of destinations
-                                    // on the back stack as users select items
-                                    popUpTo(navController.graph.findStartDestination().id) {
-                                        saveState = true
-                                    }
-                                    // Avoid multiple copies of the same destination when
-                                    // reselecting the same item
-                                    launchSingleTop = true
-                                    // Restore state when reselecting a previously selected item
-                                    restoreState = true
-                                }
-                            }
-                        )
-                    }
-                }
+                if (showBottomBar)
+                    BottomBarSession(navController = navController)
             }
         ) { innerPadding ->
 
@@ -124,7 +92,48 @@ fun MainScreen() {
              }
          }
      }*/
+}
 
+@Composable
+fun BottomBarSession(navController: NavHostController) {
+
+    val bottomNavRouteList = listOf(
+        BottomNavRoute.Home,
+        BottomNavRoute.Favourite
+    )
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    NavigationBar {
+        bottomNavRouteList.forEach { screen ->
+            NavigationBarItem(
+                selected = currentDestination?.route == screen.route,
+                icon = {
+                    Icon(
+                        imageVector = getBottomNavIconForScreen(screen = screen.route),
+                        contentDescription = screen.route
+                    )
+                },
+                label = { Text(text = screen.route) },
+                onClick = {
+                    navController.navigate(screen.route) {
+                        // Pop up to the start destination of the graph to
+                        // avoid building up a large stack of destinations
+                        // on the back stack as users select items
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        // Avoid multiple copies of the same destination when
+                        // reselecting the same item
+                        launchSingleTop = true
+                        // Restore state when reselecting a previously selected item
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
 }
 
 @Composable
@@ -149,5 +158,4 @@ enum class MovieScreenRoute(val route: String) {
     Detail(route = "detail/{movieId}"),
     Favourite(route = "favourite"),
     SeeMore(route = "seeMore")
-
 }
